d5349db1d7d38d7d561d9c75e50ffc14b36c1b3b
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-08 18:34:05 +0100

    Update report template

--- templates/report.template.r
+++ templates/report.template.r
@@ -1,9 +1,10 @@
 require(lattice)
+lattice.options(default.theme = standard.theme(color = FALSE))
 
 pdf("report.pdf")
 
 equi <- read.csv("data/equilibriation.csv", header=T)
-plot(equi$x, equi$y, xlab="Spin flips", ylab="Energy", type="n")
+plot(equi$x, equi$y, xlab="Monte Carlo moves", ylab="Magnetization", type="n")
 lines(equi$x, equi$y)
 
 %s
