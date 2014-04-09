1ed46be533195a27bc3ceb52e522f1f7924a855f
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-09 23:11:29 +0100

    A lot of rewrites

--- Ising2D.py
+++ Ising2D.py
@@ -7,126 +7,119 @@ import util
 
 from lattice import Lattice2D
 
+#General model properties
 LATTICE_SIZE = 10
-EQUILIBRIATION_SWEEPS = 30
-MEASUREMENT_SWEEPS = 50
-MEASUREMENT_FREQUENCY = 10
-MAGNETIC_FIELD = 0.1
+MAGNETIC_FIELD = 0
+
+#Measurement properties
+EQUILIBRIATION_SWEEPS = 20
+MEASUREMENT_SWEEPS = 20
+SAMPLING_FREQUENCY = 10
+
+#Temperature dependence measurements
+STARTING_TEMPERATURE = 0.01
+FINAL_TEMPERATURE = 3
+TEMPERATURE_STEP = 0.1
 
 class Ising2D:
 
-    lattice = None
-    T = 0.01
-    measurement_data = {"M" : [],
-                        "E" : []}
+    _lattice = None
 
-    def __init__(self):
+    def old__init__(self):
         results = {}
         step = 0.1
-        maxtemp = 5
-        self.lattice = Lattice2D(LATTICE_SIZE, B=MAGNETIC_FIELD)
-        while self.T < maxtemp:
-            util.write_lattice(self.lattice, "data/lattice0.csv")
-            self.equilibriate()
+        maxtemp = 3
+        self._lattice = Lattice2D(LATTICE_SIZE, B=MAGNETIC_FIELD, T=STARTING_TEMPERATURE)
+        self.equilibriate(20)
+        while self._lattice.T < maxtemp:
+            print("T: {temp}".format(temp=self._lattice.T))
+            self.equilibriate(20)
             self.generate_samples()
-            results[self.T] = self.calculate_properties()
-            util.generate_report(
-                "reports/report-T{0:g}-B{1:g}.pdf".format(self.T, MAGNETIC_FIELD)
-                )
-            self.T += step
-        self.write_results(results)
+            results[self._lattice.T] = self.calculate_properties()
+            self._lattice.T += step
+            self.measurement_data = {"M" : [],
+                                     "E" : []}
+        self.write_results(results, "results.csv")
+        util.generate_report(
+                "reports/{length}x{length}-B{B}-report.pdf".format(
+                    length=self._lattice.length,
+                    B=MAGNETIC_FIELD))
 
-    def equilibriate(self):
+    def __init__(self):
+
+        #Initialize a new lattice
+        self._lattice = Lattice2D(length=LATTICE_SIZE,
+                                 T=STARTING_TEMPERATURE,
+                                 B=MAGNETIC_FIELD)
+        results = {}
+        #Equilibriate lattice before taking averages
+        self.equilibriate(LATTICE_SIZE)
+        #Start measuring averages at increasing temperatures
+        while self._lattice.T < FINAL_TEMPERATURE:
+            print("T: {temp}".format(temp=self._lattice.T))
+            #Equilibriate lattice after temperature increase
+            #Equilibriation can be smaller because starting point is no longer
+            #at infinite temperature
+            self.equilibriate(LATTICE_SIZE / 2)
+            #Sample equilibrium properties
+            data = self.sample()
+            #Calculate averages and response functions
+            results[self._lattice.T] = self.calculate_properties(data)
+            #Write results data to disk
+            util.write_results(results, "{0}x{0}-B{1}".format(self._lattice.length, MAGNETIC_FIELD))
+            #Generate a PDF report
+            #self.gen
+            #Increase temperature for next measurement cycle
+            self._lattice.set_temperature(self._lattice.T + TEMPERATURE_STEP)
+
+    def equilibriate(self, cycles):
         """Equlilibriate the lattice before sampling."""
 
-        print("Equilibriating...")
         #Step counter
         n = 0
         #Equilibriation log for tracking magnetization
         log = [("x", "y",)]
-        for sweep in range(EQUILIBRIATION_SWEEPS):
-            for i in range(self.lattice.length**2):
+        for sweep in range(cycles):
+            for i in range(self._lattice.length**2):
                 #Perform MC step
-                self.MCStep()
+                self._lattice.mc_move()
                 n += 1
                 #Log magnetization
-                log.append((str(n), str(self.lattice.get_magnetization()),))
+                log.append((str(n), str(self._lattice.get_magnetization()),))
             #Take a snapshot of the lattice after each sweep
-            #util.write_lattice(self.lattice, "data/lattice%i.csv" % (sweep + 1))
+            #util.write_lattice(self._lattice, "data/lattice%i.csv" % (sweep + 1))
         #Write equilibriation log to disk
-        logfile = open("data/equilibriation.csv", "w")
-        for line in log:
-            logfile.write("%s,%s\n" % line)
+        util.write_log(log, "{0}x{0}-B{1}".format(self._lattice.length, MAGNETIC_FIELD), "equilibriation-T{0}.csv".format(self._lattice.T))
 
-    def generate_samples(self):
-
-        print("Calculating ensemble averages...")
-        for n in range(MEASUREMENT_SWEEPS * self.lattice.length**2):
-            self.MCStep()
-            if n % MEASUREMENT_FREQUENCY == 0:
-                self.sample()
-
-    def write_results(self, data):
-        results_file = open("results.csv", "w")
-        results_file.write("T,E,M,Xt,Xb\n")
-        for temperature in data:
-            Xt = (data[temperature]["E2"] - data[temperature]["E"]**2) / float(temperature)**2
-            Xb = (data[temperature]["M2"] - data[temperature]["M"]**2) / float(temperature)
-            line = "{T},{E},{M},{Xt},{Xb}\n".format(T=temperature,
-                                                    E=data[temperature]["E"],
-                                                    M=data[temperature]["M"],
-                                                    Xt=Xt,
-                                                    Xb=Xb)
-            results_file.write(line)
-        results_file.close()
-
-    def calculate_properties(self):
+    def calculate_properties(self, data):
         E = 0.0
         E2 = 0.0
         M = 0.0
         M2 = 0.0
-        for e in self.measurement_data["E"]:
+        for e in data["E"]:
             E += e
             E2 += e**2
-        E = E / len(self.measurement_data["E"])
-        E2 = E2 / len(self.measurement_data["E"])
-        for m in self.measurement_data["M"]:
+        E = E / len(data["E"])
+        E2 = E2 / len(data["E"])
+        for m in data["M"]:
             M += m
             M2 += m**2
-        M = M / len(self.measurement_data["M"])
-        M2 = M2 / len(self.measurement_data["M"])
+        M = M / len(data["M"])
+        M2 = M2 / len(data["M"])
         return {"E" : E, "E2" : E2, "M" : M, "M2" : M2}
 
     def sample(self):
         """Sample equilibrium properties from current configuration."""
 
-        magnetization = self.lattice.get_magnetization()
-        energy = self.lattice.get_energy()
-        self.measurement_data["M"].append(magnetization)
-        self.measurement_data["E"].append(energy)
-
-    def MCStep(self):
-        """Perform a single Monte Carlo step."""
-
-        #Select a spin at random
-        x = random.randrange(self.lattice.length)
-        y = random.randrange(self.lattice.length)
-        #Flip the spin
-        new_lattice = self.lattice.flip(x, y)
-        #Calculate the energy difference between the new
-        #and the old configuration
-        dE = new_lattice.get_energy_at(x, y) - self.lattice.get_energy_at(x, y)
-        #Calculate acceptance rate
-        if self.T > 0:
-            acc = min(1, math.exp(-dE/self.T))
-        else:
-            if dE <= 0:
-                acc = 1
-            else:
-                acc = 0
-        #Determine if the step was accepted
-        if random.uniform(0, 1) < acc:
-            self.lattice = new_lattice
+        data = {"E" : [], "M" : []}
+        for n in range(MEASUREMENT_SWEEPS * self._lattice.length**2):
+            self._lattice.mc_move()
+            if n % SAMPLING_FREQUENCY == 0:
+                magnetization = self._lattice.get_magnetization()
+                energy = self._lattice.get_energy()
+                data["M"].append(magnetization)
+                data["E"].append(energy)
+        return data
 
 if __name__ == "__main__":
     Ising2D()
