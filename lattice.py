7aebceac46fc1899948948be40421df4bc9625e3
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-09 23:10:59 +0100

    Move MC step function into Lattice2D

--- lattice.py
+++ lattice.py
@@ -1,5 +1,7 @@
 import random
-from copy import deepcopy
+import math
+#from copy import deepcopy
+from util import deepcopy
 
 class Lattice2D:
 
@@ -11,26 +13,30 @@ class Lattice2D:
     #Default: no field
     B = 0
 
+    #Temperature
+    #Default: 0.01
+    T = 0.01
+
     #Coupling between NN spins
-    #-1 - ferromagnet, aligned spins lower in energy
-    #+1 - antiferromagnet, antialigned spins lower in energy
-    #Default: ferromagnet
-    J = -1
+    #- - ferromagnet, aligned spins lower in energy
+    #+ - antiferromagnet, antialigned spins lower in energy
+    #Default: ferromagnet, multiplied by 0.5 to avoid double counting
+    J = -1 * 0.5
 
     #Calculated lattice properties
     energy = None
     magnetization = None
 
-    def __init__(self, length=None, lattice=None, B=None):
+    def __init__(self, length=None, B=None, T=None, J=None):
         #If length is specified, initialize a new lattice with the required length
         if length:
             self.initialize(length)
-        #If an existing lattice is provided, use that to initialize
-        if lattice:
-            self.lattice = lattice
-            self.length = len(lattice)
         if B:
             self.B = B
+        if J:
+            self.J = J
+        if T:
+            self.T = T
 
     def initialize(self, length):
         """Create a square lattice and populate it with random spins."""
@@ -43,6 +49,9 @@ class Lattice2D:
                 row.append(random.choice([-1, 1]))
             self.lattice.append(row)
 
+    def set_temperature(self, temperature):
+        self.T = temperature
+
     def visualize(self):
         """Print out the lattice."""
 
@@ -54,11 +63,15 @@ class Lattice2D:
         """Flip spin at (x, y)."""
 
         #Copy lattice
-        newlattice = deepcopy(self.lattice)
+        #newlattice = deepcopy(self.lattice)
         #Flip the spin
-        newlattice[y][x] = -newlattice[y][x]
+        #newlattice[y][x] = -newlattice[y][x]
         #Return a new Lattice2D object
-        return Lattice2D(lattice=newlattice, B=self.B)
+        #return Lattice2D(lattice=newlattice, B=self.B)
+
+        self.lattice[y][x] = -self.lattice[y][x]
+        self.energy = None
+        self.magnetization = None
 
     def randflip(self, x, y):
         """Randomize spin at (x, y)."""
@@ -69,6 +82,30 @@ class Lattice2D:
         newlattice[y][x] = random.choice([-1, 1])
         #Return a new Lattice2D object
         return Lattice2D(lattice=newlattice, B=self.B)
+
+    def mc_move(self):
+
+        #Select a spin at random
+        x = random.randrange(self.length)
+        y = random.randrange(self.length)
+        energy = self.get_energy_at(x, y)
+        #Flip the spin
+        self.flip(x, y)
+        #Calculate the energy difference between the new
+        #and the old configuration
+        dE = self.get_energy_at(x, y) - energy
+        #print(dE, self.T, math.exp(-dE/self.T))
+        #Calculate acceptance rate
+        if self.T > 0:
+            acc = min(1, math.exp(-dE/self.T))
+        else:
+            if dE <= 0:
+                acc = 1
+            else:
+                acc = 0
+        #If the step was rejected, flip the spin back to it's original state
+        if random.uniform(0, 1) > acc:
+            self.flip(x, y)
 
     def get_magnetization(self):
         """Returns total magnetization of the current configuration."""
@@ -95,14 +132,14 @@ class Lattice2D:
                     #Nearest-neighbour interactions
                     #Negative coordinates used to get periodic boundary
                     #conditions for free thanks to Python's lists
-                    energy += 0.5 * self.lattice[-y][-x]\
-                                  * self.lattice[-y + 1][-x] * self.J
-                    energy += 0.5 * self.lattice[-y][-x]\
-                                  * self.lattice[-y - 1][-x] * self.J
-                    energy += 0.5 * self.lattice[-y][-x]\
-                                  * self.lattice[-y][-x + 1] * self.J
-                    energy += 0.5 * self.lattice[-y][-x]\
-                                  * self.lattice[-y][-x - 1] * self.J
+                    energy += self.lattice[-y][-x]\
+                                * self.lattice[-y + 1][-x] * self.J
+                    energy += self.lattice[-y][-x]\
+                                * self.lattice[-y - 1][-x] * self.J
+                    energy += self.lattice[-y][-x]\
+                                * self.lattice[-y][-x + 1] * self.J
+                    energy += self.lattice[-y][-x]\
+                                * self.lattice[-y][-x - 1] * self.J
                     #Interactions with the magnetic field
                     energy += self.lattice[-y][-x] * self.B
             #Store energy for future use
@@ -116,18 +153,19 @@ class Lattice2D:
         energy = 0.0
 
         #NN interactions with PBC
-        energy += 0.5 * self.lattice[y][x]\
-                      * self.lattice[min(y + 1, y + 1 - self.length)][x]\
-                      * self.J
-        energy += 0.5 * self.lattice[y][x]\
-                      * self.lattice[y - 1][x]\
-                      * self.J
-        energy += 0.5 * self.lattice[y][x]\
-                      * self.lattice[y][min(x + 1, x + 1 - self.length)]\
-                      * self.J
-        energy += 0.5 * self.lattice[y][x]\
-                      * self.lattice[y][x - 1]\
-                      * self.J
+        energy += self.lattice[y][x]\
+                    * self.lattice[min(y + 1, y + 1 - self.length)][x]\
+                    * self.J
+        energy += self.lattice[y][x]\
+                    * self.lattice[y - 1][x]\
+                    * self.J
+        energy += self.lattice[y][x]\
+                    * self.lattice[y][min(x + 1, x + 1 - self.length)]\
+                    * self.J
+        energy += self.lattice[y][x]\
+                    * self.lattice[y][x - 1]\
+                    * self.J
+
         #Interactions with the magnetic field
         energy += self.lattice[y][x] * self.B
 
