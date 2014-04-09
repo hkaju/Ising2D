0400a5dce08b7f5ac0275c3c04ad38011b4a0bff
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-09 23:13:00 +0100

    Add more helper functions

--- util.py
+++ util.py
@@ -1,7 +1,31 @@
 import glob
 import subprocess
+import os
+
+def deepcopy(lattice):
+    l = len(lattice)
+    newlattice = [[None] * l] * l
+    for x in range(l):
+        for y in range(l):
+            newlattice[y][x] = 1 * lattice[y][x]
+    return newlattice
 
 def generate_report(filename):
+    report_template = open("templates/report.template.r", 'r').read()
+    equilibriation_template = """{run} <- read.csv("{filename}", header=T)
+plot({run}$x, {run}$y, xlab="MC moves", ylab="Magnetization", type="n", main="{run}", ylim=c(-1, 1))
+lines({run}$x, {run}$y)\n"""
+    equilibriation_graphs = ""
+    for datafile in glob.glob("data/equilibriation*.csv"):
+        run = datafile[20:-4]
+        equilibriation_graphs += equilibriation_template.format(filename=datafile, run=run)
+    #Write R file to disk
+    open('report.r', 'w').write(report_template.format(equilibriation_graphs=equilibriation_graphs, filename=filename))
+    #Run R and compile report
+    subprocess.call(['R', '-f report.r'])
+    print("Report generated!")
+
+def generate_equilibriation_report(filename):
     '''Generate a PDF report containing magnetization data and lattice snapshots.'''
 
     n_lattices = len(glob.glob("data/lattice*.csv"))
@@ -29,3 +53,29 @@ def write_lattice(lattice, filename):
         for y in range(lattice.length):
             f.write("%i,%i,%i\n" % (x, y, lattice.lattice[y][x]))
     f.close()
+
+def write_results(data, folder):
+    path = "data" + os.sep + folder
+    if not os.path.exists(path):
+        os.mkdir(path)
+    results_file = open(path + os.sep + "results.csv", "w")
+    results_file.write("T,E,M,Xt,Xb\n")
+    for temperature in data:
+        Xt = (data[temperature]["E2"] - data[temperature]["E"]**2) / float(temperature)**2
+        Xb = (data[temperature]["M2"] - data[temperature]["M"]**2) / float(temperature)
+        line = "{T},{E},{M},{Xt},{Xb}\n".format(T=temperature,
+                                                E=data[temperature]["E"],
+                                                M=data[temperature]["M"],
+                                                Xt=Xt,
+                                                Xb=Xb)
+        results_file.write(line)
+    results_file.close()
+
+def write_log(log, folder, filename):
+    path = "data" + os.sep + folder
+    if not os.path.exists(path):
+        os.mkdir(path)
+    logfile = open(path + os.sep + filename, "w")
+    for line in log:
+        logfile.write("%s,%s\n" % line)
+    logfile.close()
