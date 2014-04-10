406126342a5d010c26068241f8160835054f9969
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-10 01:10:05 +0100

    Clean up Lattice2D

--- lattice.py
+++ lattice.py
@@ -1,7 +1,5 @@
 import random
 import math
-#from copy import deepcopy
-from util import deepcopy
 
 class Lattice2D:
 
@@ -27,29 +25,32 @@ class Lattice2D:
     energy = None
     magnetization = None
 
-    def __init__(self, length=None, B=None, T=None, J=None):
-        #If length is specified, initialize a new lattice with the required length
-        if length:
-            self.initialize(length)
+    def __init__(self, length, B=None, T=None, J=None):
         if B:
             self.B = B
         if J:
             self.J = J
         if T:
             self.T = T
+        if length:
+            self.initialize(length)
 
     def initialize(self, length):
         """Create a square lattice and populate it with random spins."""
 
         self.length = length
-        print("Initializing {len}x{len} square lattice".format(len=length))
-        for i in range(length):
-            row = []
-            for r in range(length):
-                row.append(random.choice([-1, 1]))
-            self.lattice.append(row)
+        #Initialize lattice with empty rows. Minor performance gain vs appending
+        self.lattice = [None] * length
+        for y in range(length):
+            row = [None] * length
+            for x in range(length):
+                row[x] = random.choice([-1, 1])
+            self.lattice[y] = row
+        print("Initialized {0}x{0} lattice with T={1}, B={2}, J={3}".format(self.length, self.T, self.B, self.J))
 
     def set_temperature(self, temperature):
+        """Set the temperature of the lattice."""
+
         self.T = temperature
 
     def visualize(self):
@@ -62,39 +63,26 @@ class Lattice2D:
     def flip(self, x, y):
         """Flip spin at (x, y)."""
 
-        #Copy lattice
-        #newlattice = deepcopy(self.lattice)
         #Flip the spin
-        #newlattice[y][x] = -newlattice[y][x]
-        #Return a new Lattice2D object
-        #return Lattice2D(lattice=newlattice, B=self.B)
-
         self.lattice[y][x] = -self.lattice[y][x]
-        self.energy = None
-        self.magnetization = None
 
-    def randflip(self, x, y):
-        """Randomize spin at (x, y)."""
-
-        #Copy lattice
-        newlattice = deepcopy(self.lattice)
-        #Generate new spin
-        newlattice[y][x] = random.choice([-1, 1])
-        #Return a new Lattice2D object
-        return Lattice2D(lattice=newlattice, B=self.B)
+        #Reset properties so that they will be recalculated
+        #self.energy = None
+        #self.magnetization = None
 
     def mc_move(self):
+        """Perform a single Monte Carlo move."""
 
         #Select a spin at random
         x = random.randrange(self.length)
         y = random.randrange(self.length)
+        #Calculate energy of the old configuration
         energy = self.get_energy_at(x, y)
         #Flip the spin
         self.flip(x, y)
         #Calculate the energy difference between the new
         #and the old configuration
         dE = self.get_energy_at(x, y) - energy
-        #print(dE, self.T, math.exp(-dE/self.T))
         #Calculate acceptance rate
         if self.T > 0:
             acc = min(1, math.exp(-dE/self.T))
@@ -108,7 +96,7 @@ class Lattice2D:
             self.flip(x, y)
 
     def get_magnetization(self):
-        """Returns total magnetization of the current configuration."""
+        """Returns magnetization per site of the current configuration."""
 
         #If no magnetization is stored, recalculate
         if not self.magnetization:
