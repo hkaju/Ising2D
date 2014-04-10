7b9e456b8709ba25a4cf7f1cdd6555fbd4dbb6d5
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-10 01:25:15 +0100

    Update template file

--- templates/report.template.r
+++ templates/report.template.r
@@ -1,6 +1,6 @@
-pdf("{filename}")
+pdf("reports/{run}.pdf")
 
-data <- read.csv("results.csv", header=T)
+data <- read.csv("data/{run}/results.csv", header=T)
 
 par(mfrow=c(2,2))
 plot(data$T, data$M, xlab="Temperature", ylab="Magnetization")
@@ -8,7 +8,7 @@ plot(data$T, data$E, xlab="Temperature", ylab="Energy")
 plot(data$T, data$Xb, xlab="Temperature", ylab="Magnetic susceptibility")
 plot(data$T, data$Xt, xlab="Temperature", ylab="Heat capacity")
 
-par(mfrow=c(2,3))
+par(mfrow=c(3,2))
 {equilibriation_graphs}
 
 dev.off()
