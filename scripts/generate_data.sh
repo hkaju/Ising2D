2b578cdfa5f5bb375efdf031e832825d9b380efb
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-05-01 22:46:52 +0100

    Add some utility scripts

---
+++ scripts/generate_data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+./Ising2D 5x5-B0
+./Ising2D 10x10-B0
+./Ising2D 20x20-B0
+./Ising2D 30x30-B0
+./Ising2D 40x40-B0
+./Ising2D 50x50-B0
+./Ising2D 60x60-B0
+./Ising2D 70x70-B0
+./Ising2D 80x80-B0
+./Ising2D 90x90-B0
+./Ising2D 100x100-B0
